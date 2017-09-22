a870d081c26f4248ae484789091864dd63409144
Carlos Saltijeral <carlossaltrev@gmail.com>
2017-09-22 18:28:39 -0500

    Add JS compression support

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
- 
+const uglify = require('gulp-uglify');
+
 gulp.task('sass', () => {
-  return gulp.src('./sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./public/css'));
+    return gulp.src('./sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('default', ['sass'], () => {
+gulp.task('js', () => {
+    return gulp.src('./js/user.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('default', ['sass', 'js'], () => {
     gulp.watch('./sass/**/*.sass', ['sass']);
+    gulp.watch('./js/**/*.js', ['js']);
 });
