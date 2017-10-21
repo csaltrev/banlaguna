3f289e6fe47030f3d072aeadbe33dfea3f2b0fdb
Carlos Saltijeral <carlossaltrev@gmail.com>
2017-10-21 12:36:30 -0500

    Add user transactions support for admin

--- js/admin.js
+++ js/admin.js
@@ -6,6 +6,51 @@
         const accountsModal = document.querySelector('#accounts-modal');
         const userModal = document.querySelector('#user-modal');
         const newAccountModal = document.querySelector('#new-account-modal');
+        const userModalTbody = document.querySelector('#user-modal table tbody');
+        const userModalTitle = document.querySelector('#user-modal p');
+
+        const formatDateTime = timestamp => {
+            const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+            const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+            const dateTime = new Date(timestamp);
+            const dateTimeHour = dateTime.getHours();
+            const dateTimeMinute = dateTime.getMinutes();
+
+            const day = days[dateTime.getDay()];
+            const date = `${months[dateTime.getMonth()]} ${dateTime.getDate()}, ${dateTime.getFullYear()}`;
+
+            const hour = dateTimeHour < 10 ? `0${dateTimeHour}` : dateTimeHour;
+            const minute = dateTimeMinute < 10 ? `0${dateTimeMinute}` : dateTimeMinute;
+            const time = `${hour}:${minute}`;
+
+            return `${day}, ${date} @ ${time}`;
+        };
+
+        const populateTransactionsTable = transactions => {
+            transactions.forEach(transaction => {
+                const tr = document.createElement('tr');
+                const idTh = document.createElement('th');
+                const receiverTd = document.createElement('td');
+                const conceptTd = document.createElement('td');
+                const dateTd = document.createElement('td');
+                const quantityTd = document.createElement('td');
+
+                idTh.textContent = transaction.id;
+                receiverTd.textContent = transaction.receiver;
+                conceptTd.textContent = transaction.concept;
+                dateTd.textContent = formatDateTime(transaction.timestamp);
+                quantityTd.textContent = parseFloat(transaction.quantity).toFixed(2);
+
+                tr.appendChild(idTh);
+                tr.appendChild(receiverTd);
+                tr.appendChild(conceptTd);
+                tr.appendChild(dateTd);
+                tr.appendChild(quantityTd);
+
+                userModalTbody.appendChild(tr);
+            });
+        };
 
         const handleOpenModal = modal => {
             modal.classList.add('is-active');
@@ -13,11 +58,27 @@
                 if (e.target.id === 'cancel-btn' ||
                     e.target.id === 'close-btn') {
                     modal.classList.remove('is-active');
+                    if (modal.id === 'user-modal') {
+                        userModalTbody.innerHTML = '';
+                        userModalTitle.textContent = 'Usuario';
+                    }
                 }
             });
         };
 
         transactionsSection.addEventListener('click', e => {
+            if (e.target.id.startsWith('user-')) {
+                const userId = e.target.id.substr(5);
+
+                fetch(`/admin/user/${userId}`).then(res => {
+                    return res.json();
+                }).then(transactions => {
+                    userModalTitle.textContent = e.target.textContent;
+                    handleOpenModal(userModal);
+                    populateTransactionsTable(transactions);
+                });
+            }
+
             switch (e.target.id) {
                 case 'bulk-btn':
                     handleOpenModal(bulkModal);
@@ -34,10 +95,6 @@
                             handleOpenModal(newAccountModal);
                         }
                     });
-                    e.stopPropagation();
-                    break;
-                case 'user':
-                    handleOpenModal(userModal);
                     e.stopPropagation();
                     break;
                 default:
--- routes/admin.js
+++ routes/admin.js
@@ -5,9 +5,46 @@ const router = new Router();
 module.exports = router;
 
 router.get('/', async(req, res, next) => {
+    const sess = req.session;
+    if (sess.userId && sess.username === 'admin') {
+        try {
+            const adminQuery = 'SELECT * FROM public.accounts WHERE username = $1;';
+            const adminRes = await db.query(adminQuery, ['admin']);
+            const admin = adminRes.rows[0];
+            admin.balance = parseFloat(admin.balance).toFixed(2);
+
+            const usersQuery = 'SELECT * FROM public.accounts;';
+            const usersRes = await db.query(usersQuery);
+            const users = usersRes.rows;
+
+            const formattedUsers = users;
+            formattedUsers.forEach(user => {
+                user.balance = parseFloat(user.balance).toFixed(2);
+            });
+
+            const data = {
+                admin: admin,
+                users: formattedUsers
+            }
+
+            res.render('admin', data);
+        } catch (e) {
+            res.send(e.stack);
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.get('/user/:id', async (req, res, next) => {
+    const userId = req.params.id;
     try {
-        res.render('admin');
+        const transactionsQuery = 'SELECT * FROM public.transactions WHERE sender = $1;';
+        const transactionsRes = await db.query(transactionsQuery, [userId]);
+        const userTransactions = transactionsRes.rows;
+
+        res.send(userTransactions);
     } catch (e) {
-        res.send(e);
+        res.send(e.stack);
     }
 });
