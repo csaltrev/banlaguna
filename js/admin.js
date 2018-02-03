88874bb517d3d216df8314ab2fa3dfed10c0f6bd
Carlos Saltijeral <carlossaltrev@gmail.com>
2018-02-03 10:32:54 -0600

    Run Gulp compilation

--- js/admin.js
+++ js/admin.js
@@ -103,8 +103,8 @@
                     link.setAttribute('href', encodedUri);
 
                     let today = new Date();
-                    const dd = today.getDate();
-                    const mm = today.getMonth() + 1;
+                    let dd = today.getDate();
+                    let mm = today.getMonth() + 1;
                     const yyyy = today.getFullYear();
                     if(dd < 10) dd = '0' + dd;
                     if(mm < 10) mm = '0' + mm;
